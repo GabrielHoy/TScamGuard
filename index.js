2eeb2e52d3e4435c3beda052e6cfef508c0ab1cf
GabrielHoy <79767159+GabrielHoy@users.noreply.github.com>
2021-11-08 10:20:18 -0500

    begin axios impl

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ bot.on("error", (err) => {
   console.log(err);
 });
 
+//thanks stack overflow
 let urlAwfulRegex = /((?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?)/gi;
 
 const hardDomainBlacklist = {
@@ -123,8 +124,10 @@ const IsInfringingMessage = (message) => {
     //Soft checks(Should stay generally up-to-speed with scams as they progress)
       //Gets a list of all the links in the message, iterates through them
       for (let linkText of (message.content.toLowerCase().match(urlAwfulRegex) || [])) {
+        let originalLink = linkText;
+        console.warn(`Checking link ${originalLink}`);
         //Strip url of http for use later
-        linkText = linkText.replace("https://", "").replace("http://", "").replace("www.", "")
+        linkText = linkText.replace("https://", "").replace("http://", "").replace("www.", "");
         //strip url of domain extension
         linkText = linkText.replace(/\.[^.]+$/, "");
 
